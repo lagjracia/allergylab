2dda7933511ae4f010e5c671897027b74eb4f524
lagjracia <lerinoso@yahoo.com>
2026-04-26 18:56:08 +0100

    Push to Docker

--- Part5/db-loader/dbconnect.py
+++ Part5/db-loader/dbconnect.py
@@ -1,12 +1,16 @@
 import mysql.connector
+import os
+db_name = os.environ['DB_NAME']
+dbUser = os.environ['DB_USER']
+dbPass = os.environ['DB_PASS']
 
 def connection():
     # Edited out actual values
     conn = mysql.connector.connect( host='dbhost',
                             port=3306,
-                            database='allergen_info',
-                            user='pythonapp',
-                            password='secretPass123!')
+                            database=db_name,
+                            user=dbUser,
+                            password=dbPass)
     c = conn.cursor()
 
     return c, conn
